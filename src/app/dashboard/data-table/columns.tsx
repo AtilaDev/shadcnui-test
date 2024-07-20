3628ae891bc8591fc2e3ef1a20da57b012d31b23
Leandro <atiladevelop@gmail.com>
2024-07-20 01:56:43 -0300

    Visibility

--- src/app/dashboard/data-table/columns.tsx
+++ src/app/dashboard/data-table/columns.tsx
@@ -1,26 +1,145 @@
 'use client';
 
-import { ColumnDef } from '@tanstack/react-table';
+import { ColumnDef, SortDirection } from '@tanstack/react-table';
 import { Payment } from '@/data/payments.data';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { ArrowDown, ArrowUp, ArrowUpDown, MoreHorizontal } from 'lucide-react';
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { toast } from 'sonner';
+
+const statusToVariant = {
+  pending: 'info' as const,
+  processing: 'info' as const,
+  failed: 'destructive' as const,
+  default: 'success' as const,
+};
+
+const SortedIcon = ({ isSorted }: { isSorted: SortDirection | boolean }) => {
+  if (isSorted === 'asc') {
+    return <ArrowUp className='ml-2 h-4 w-4' />;
+  }
+  if (isSorted === 'desc') {
+    return <ArrowDown className='ml-2 h-4 w-4' />;
+  }
+
+  return <ArrowUpDown className='ml-2 h-4 w-4' />;
+};
 
 export const columns: ColumnDef<Payment>[] = [
   {
-    accessorKey: 'status',
-    header: 'Status',
+    accessorKey: 'clientName',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Client Name
+          <SortedIcon isSorted={column.getIsSorted()} />
+        </Button>
+      );
+    },
   },
   {
-    accessorKey: 'email',
-    header: 'Email',
+    accessorKey: 'status',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Status
+          <SortedIcon isSorted={column.getIsSorted()} />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const status = row.getValue('status') as keyof typeof statusToVariant;
+      const variant = statusToVariant[status] || statusToVariant.default;
+      return (
+        <Badge variant={variant} capitalize>
+          {status}
+        </Badge>
+      );
+    },
   },
   {
     accessorKey: 'amount',
-    header: 'Amount',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Amount
+          <SortedIcon isSorted={column.getIsSorted()} />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue('amount'));
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(amount);
+
+      return <div className='text-right font-medium'>{formatted}</div>;
+    },
   },
   {
-    accessorKey: 'clientName',
-    header: 'Client Name',
+    accessorKey: 'email',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Email
+          <SortedIcon isSorted={column.getIsSorted()} />
+        </Button>
+      );
+    },
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost' className='h-8 w-8 p-0'>
+              <span className='sr-only'>Open menu</span>
+              <MoreHorizontal className='h-4 w-4' />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => {
+                navigator.clipboard.writeText(payment.id);
+                toast.success('Payment ID copied to clipboard', {
+                  position: 'top-right',
+                });
+              }}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import { Toaster as SonnerToaster } from '@/components/ui/sonner';
-import { Toaster } from '@/components/ui/toaster';
+// import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
 import { cn } from '@/lib/utils';
 
@@ -29,8 +29,8 @@ export default function RootLayout({
         )}
       >
         {children}
-        <SonnerToaster />
-        <Toaster />
+        <SonnerToaster richColors />
+        {/* <Toaster /> */}
       </body>
     </html>
   );
